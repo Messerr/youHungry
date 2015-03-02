a610a2a97afee0b5b9af2f1ee466cc8af3024f2f
xTrebliG <gil@crankshiftnyc.com>
2015-03-02 15:21:36 -0500

    Added user model details

--- db/migrate/20150302200334_create_users.rb
+++ db/migrate/20150302200334_create_users.rb
@@ -1,7 +1,11 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-
+      t.string "fname"
+      t.string "lname"
+      t.text "bio"
+      t.string "email"
+      t.string "password"
       t.timestamps null: false
     end
   end
