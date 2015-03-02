e1294d113b4c63e06946dec377909c6461860d2d
Cameron Snyder <Dooffycom@gmail.com>
2015-03-02 16:06:08 -0500

    added address, longitude and latitude to database users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150302200334) do
+ActiveRecord::Schema.define(version: 20150302210453) do
 
   create_table "users", force: :cascade do |t|
     t.string   "fname"
@@ -21,6 +21,9 @@ ActiveRecord::Schema.define(version: 20150302200334) do
     t.string   "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.float    "latitude"
+    t.float    "longitude"
+    t.string   "address"
   end
 
 end
