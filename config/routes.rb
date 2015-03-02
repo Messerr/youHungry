e097a7a5c559faab4b658d2f93bd6ae527b9941b
messerr <dmesser@me.com>
2015-03-02 17:22:34 -0500

    User signup, logout, login functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,14 @@
 Rails.application.routes.draw do
   
-  root 'welcome#index'
+resources :users
+
+
+  get '/signin', to: 'sessions#new'
+  post '/signin', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
+
+  root 'users#index'
+
+
 
 end
