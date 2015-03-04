229bcf78cd862340bcbe6c1a3a7b82712957417f
xTrebliG <gil@crankshiftnyc.com>
2015-03-04 10:56:00 -0500

    Home page styles

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,10 +6,11 @@ class WelcomeController < ApplicationController
   def new
   end
 
+
   def edit
   end
 
   def show
   end
-  
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
 
 	geocoded_by :address
 	after_validation :geocode
-
+  validates_presence_of :email, :password
 	has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "/images/:style/missing.png"
   	validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
